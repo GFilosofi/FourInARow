936ed3e80650d791541adceaabe5c3b3386a6431
GFilosofi <g.filosofi@me.com>
2019-05-11 18:14:15 +0200

    Initial Commit

---
+++ FourInARow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FourInARow
+//
+//  Created by Gabriele Filosofi on 11/05/2019.
+//  Copyright © 2019 Gabriele Filosofi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
